4f215ddf7c7d947f69e8069363592399d3f3a2a0
YannisFouzi <yfouzi.dev@gmail.com>
2024-10-20 16:26:16 +0200

    test email

--- api/send-email.js
+++ api/send-email.js
@@ -1,20 +1,33 @@
 const nodemailer = require("nodemailer");
 
 module.exports = async (req, res) => {
+  console.log("Function started");
   if (req.method === "POST") {
     const { name, email, subject, message } = req.body;
+    console.log("Received data:", { name, email, subject });
 
-    let transporter = nodemailer.createTransport({
-      host: "ssl0.ovh.net",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+    let transporter;
+    try {
+      console.log("Creating transporter");
+      transporter = nodemailer.createTransport({
+        host: "ssl0.ovh.net",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+      console.log("Transporter created successfully");
+    } catch (error) {
+      console.error("Error creating transporter:", error);
+      return res
+        .status(500)
+        .json({ message: "Failed to create email transporter" });
+    }
 
     try {
+      console.log("Attempting to send email");
       await transporter.sendMail({
         from: '"Merci Lille" <contact@mercilille.com>',
         to: "contact@mercilille.com",
@@ -22,13 +35,16 @@ module.exports = async (req, res) => {
         text: `De: ${name} (${email})\n\nMessage: ${message}`,
         html: `<p><strong>De:</strong> ${name} (${email})</p><p><strong>Message:</strong> ${message}</p>`,
       });
-
+      console.log("Email sent successfully");
       res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
       console.error("Error sending email:", error);
-      res.status(500).json({ message: "Failed to send email" });
+      res
+        .status(500)
+        .json({ message: "Failed to send email", error: error.message });
     }
   } else {
+    console.log("Method not allowed:", req.method);
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
